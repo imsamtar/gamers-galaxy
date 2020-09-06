6e761c8a0ca87f6b77004f0a11efa6f3b72ad0d3
SAMEER TARIQ <imsamtar@gmail.com>
2020-09-06 21:51:53 +0500

    nicescroll

--- src/components/Header/Logo.svelte
+++ src/components/Header/Logo.svelte
@@ -9,7 +9,7 @@
 <div
   class="col-xl-4 col-lg-3 col-md-8 col-8 order-xl-1 order-lg-1 order-md-2
     order-2">
-  <a class="navbar-brand" href="index.html">
+  <a class="navbar-brand" href="/">
     <img src="assets/img/logo.png" alt="Logo" />
   </a>
 </div>
--- src/components/NiceSelect.svelte
+++ src/components/NiceSelect.svelte
@@ -4,9 +4,21 @@
   onMount(function () {
     setTimeout(() => {
       window.$("select").niceSelect();
+      window.$(".events_list-wrap").niceScroll({
+        cursorcolor: "rgb(105, 82, 253)",
+        background: "rgb(44, 49, 89)",
+        cursorborder: "0px",
+        autohidemode: "leave",
+      });
     }, 500);
     setInterval(() => {
       window.$("select").niceSelect();
+      window.$(".events_list-wrap").niceScroll({
+        cursorcolor: "rgb(105, 82, 253)",
+        background: "rgb(44, 49, 89)",
+        cursorborder: "0px",
+        autohidemode: "leave",
+      });
     }, 5000);
   });
 </script>
